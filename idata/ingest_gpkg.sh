2544a9b2a92a0f84474987f1f1e98e7546c5c612
Giuliano Langella <gyuliano@libero.it>
2025-04-02 17:08:53 +0200

    update data ingest with indicator renaturalization

--- idata/ingest_gpkg.sh
+++ idata/ingest_gpkg.sh
@@ -1,28 +1,50 @@
+#!/bin/bash
+
+alreadyDone=0
+
+alreadyDone=1
+if [ "$alreadyDone" -ne 1 ]; then
+  ogr2ogr \
+    -f "PostgreSQL" \
+    PG:"host=localhost port=5432 dbname=geoserver user=geoserver password=geoserver" \
+    regions_sdg11-3-1_2022_2023.gpkg \
+    -nln regions \
+    -overwrite
+fi
+
+alreadyDone=1
+if [ "$alreadyDone" -ne 1 ]; then
+  ogr2ogr \
+    -f "PostgreSQL" \
+    PG:"host=localhost port=5432 dbname=geoserver user=geoserver password=geoserver" \
+    regions_sdg11-3-1_2021_2022.gpkg \
+    -nln regions \
+    -append
+fi
+
+alreadyDone=1
+if [ "$alreadyDone" -ne 1 ]; then
+  ogr2ogr \
+    -f "PostgreSQL" \
+    PG:"host=localhost port=5432 dbname=geoserver user=geoserver password=geoserver" \
+    municipalities_sdg11-3-1_2022_2023.gpkg \
+    -nln municipalities \
+    -overwrite
+fi
+
+alreadyDone=1
+if [ "$alreadyDone" -ne 1 ]; then
+  ogr2ogr \
+    -f "PostgreSQL" \
+    PG:"host=localhost port=5432 dbname=geoserver user=geoserver password=geoserver" \
+    municipalities_sdg11-3-1_2021_2022.gpkg \
+    -nln municipalities \
+    -append
+fi
+
 ogr2ogr \
   -f "PostgreSQL" \
   PG:"host=localhost port=5432 dbname=geoserver user=geoserver password=geoserver" \
-  regions_sdg11-3-1_2022_2023.gpkg \
-  -nln regions \
+  municipalities_renaturalization_2022_2023.gpkg \
+  -nln renaturalization \
   -overwrite
-
-ogr2ogr \
-  -f "PostgreSQL" \
-  PG:"host=localhost port=5432 dbname=geoserver user=geoserver password=geoserver" \
-  regions_sdg11-3-1_2021_2022.gpkg \
-  -nln regions \
-  -append
-
-
-ogr2ogr \
-  -f "PostgreSQL" \
-  PG:"host=localhost port=5432 dbname=geoserver user=geoserver password=geoserver" \
-  municipalities_sdg11-3-1_2022_2023.gpkg \
-  -nln municipalities \
-  -overwrite
-
-ogr2ogr \
-  -f "PostgreSQL" \
-  PG:"host=localhost port=5432 dbname=geoserver user=geoserver password=geoserver" \
-  municipalities_sdg11-3-1_2021_2022.gpkg \
-  -nln municipalities \
-  -append
